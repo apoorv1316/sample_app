2668b62430d77f8644a83414eea3823e63df1390
apoorv1316 <tiwari.apoorv1316@gmail.com>
2020-11-17 09:48:42 +0530

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 module SessionsHelper
-
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -21,7 +20,10 @@ module SessionsHelper
       end
     end
   end
-    
+
+  def current_user?(user)
+    user && user == current_user
+  end
 
   def logged_in?
     !current_user.nil?
@@ -37,6 +39,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
